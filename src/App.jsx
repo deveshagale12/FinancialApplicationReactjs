f69f81d14e6011cabc0e274b76db5b271c017152
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-09-08 02:31:52 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+
+    return (
+        <div className="app">
+
+            <h1>Banking Platform</h1>
+
+            <p>
+                Welcome to the Banking Platform
+            </p>
+
+            <button>
+                Customer Registration
+            </button>
+
+        </div>
+    );
+}
+
+export default App;
